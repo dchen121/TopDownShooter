261493fd08aec6a6d3978ee1fe90e1afc54b5f87
Daniel Chen <chen.d__@hotmail.com>
2016-02-13 19:30:51 -0800

    set up game JFrame in main method

---
+++ src/src/Game.java
@@ -0,0 +1,14 @@
+import javax.swing.*;
+
+/**
+ * Created by Daniel on 16-02-13.
+ */
+public class Game {
+
+    public static void main(String[] args) {
+        JFrame window = new JFrame("Top Down Shooter");
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.pack();
+        window.setVisible(true);
+    }
+}
