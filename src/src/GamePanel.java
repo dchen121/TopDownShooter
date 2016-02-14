b8ac632f1928140b0a42b8aaef9ff0887afd8842
Daniel Chen <chen.d__@hotmail.com>
2016-02-13 19:35:48 -0800

    set up thread to run game loop

--- src/src/GamePanel.java
+++ src/src/GamePanel.java
@@ -4,15 +4,49 @@ import java.awt.*;
 /**
  * @author Daniel Chen
  */
-public class GamePanel extends JPanel {
+public class GamePanel extends JPanel implements Runnable {
 
     public static int WIDTH = 400;
     public static int HEIGHT = 400;
+
+    private Thread thread;
+    private boolean running;
 
     public GamePanel() {
         super();
         setPreferredSize(new Dimension(WIDTH, HEIGHT));
         setFocusable(true);
         requestFocus();
+    }
+
+    @Override
+    public void addNotify() {
+        super.addNotify();
+
+        if (thread == null) {
+            thread = new Thread(this);
+            thread.start();
+        }
+    }
+
+    /**
+     * When an object implementing interface <code>Runnable</code> is used
+     * to create a thread, starting the thread causes the object's
+     * <code>run</code> method to be called in that separately executing
+     * thread.
+     * <p/>
+     * The general contract of the method <code>run</code> is that it may
+     * take any action whatsoever.
+     *
+     * @see Thread#run()
+     */
+    @Override
+    public void run() {
+        running = true;
+
+        // GAME LOOP
+        while (running) {
+
+        }
     }
 }
